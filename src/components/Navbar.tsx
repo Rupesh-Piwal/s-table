aa0882ac663b2cc2747caa021e19fe2286dc9227
rupesh <rpiwal02@gmail.com>
2025-03-07 00:14:07 +0530

    add: navbar component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="text-[42px] font-[400] text-[#191919] text-center my-4">
+      STUDY<span className="font-[700]">table</span>
+    </div>
+  );
+};
+
+export default Navbar;
