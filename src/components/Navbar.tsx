0e3f42570bf39f685195bb0889145a034ce182c4
rupesh <rpiwal02@gmail.com>
2025-03-07 14:54:25 +0530

    feat: header mobile-view

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,21 @@
 "use client";
 
 import Link from "next/link";
+import { BookOpen } from "lucide-react";
 
 const Navbar = () => {
   return (
-    <nav className="w-full py-6">
-      <div className="container mx-auto px-4">
+    <nav className="w-full py-4 sm:py-6">
+      <div className="container mx-auto px-4 sm:px-6 lg:px-8">
         <Link href="/" className="flex justify-center items-center group">
-          <h1 className="text-4xl tracking-tight text-primary transition-colors">
+          <div className="relative overflow-hidden rounded-full bg-gradient-to-r from-[#707070] to-[#A3A3A3] p-1.5 mr-3 shadow-lg">
+            <BookOpen className="h-6 w-6 text-[#191919]" />
+          </div>
+
+          <h1 className="text-3xl sm:text-4xl tracking-tight text-[#191919] transition-colors">
             <span className="font-normal">STUDY</span>
             <span className="font-bold">table</span>
-            <span className="block h-0.5 w-0 group-hover:w-full bg-primary transition-all duration-300 ease-in-out" />
+            <span className="block h-0.5 w-0 group-hover:w-full bg-[#191919] transition-all duration-300 ease-in-out" />
           </h1>
         </Link>
       </div>
